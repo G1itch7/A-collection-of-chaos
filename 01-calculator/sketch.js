5af84c9e835e5359e0e713c4ce0882906df66b56
G1itch7 <mariosonic@hotmail.dk>
2024-04-11 01:14:06 +0200

    Made the calc, lacks functionality

--- 01-calculator/sketch.js
+++ 01-calculator/sketch.js
@@ -1,7 +1,11 @@
 function setup() {
-  createCanvas(400, 400);
+  noCanvas()
 }
 
 function draw() {
   background(220);
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight)
 }
